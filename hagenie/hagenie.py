ea4776b3fcb595424b30099f9daf53671e8dec34
Yonsm <I@YONSM.NET>
2018-03-09 12:38:51 +0800

    _url_collapse_path_split

--- hagenie/hagenie.py
+++ hagenie/hagenie.py
@@ -6,6 +6,30 @@ import os, sys, ssl
 import cgi, json
 from stat import *
 
+def _url_collapse_path_split(path):
+    path_parts = []
+    for part in path.split('/'):
+        if part == '.':
+            path_parts.append('')
+        else:
+            path_parts.append(part)
+    # Filter out blank non trailing parts before consuming the '..'.
+    path_parts = [part for part in path_parts[:-1] if part] + path_parts[-1:]
+    if path_parts:
+        tail_part = path_parts.pop()
+    else:
+        tail_part = ''
+    head_parts = []
+    for part in path_parts:
+        if part == '..':
+            head_parts.pop()
+        else:
+            head_parts.append(part)
+    if tail_part and tail_part == '..':
+        head_parts.pop()
+        tail_part = ''
+    return ('/' + '/'.join(head_parts), tail_part)
+
 class ServerHandler(CGIHTTPServer.CGIHTTPRequestHandler):
 
     def do_POST(self):
@@ -16,13 +40,15 @@ class ServerHandler(CGIHTTPServer.CGIHTTPRequestHandler):
             self.do_GET()
 
     have_fork = False
-    cgi_directories = ['/']
+    #cgi_directories = ['/']
     def is_cgi(self):
-        is_cgi = CGIHTTPServer.CGIHTTPRequestHandler.is_cgi(self)
-        if is_cgi == True:
-            pathname = '.' + self.path.split("?")[0]
-            is_cgi = os.path.isfile(pathname) and self.is_executable(pathname)
-        return is_cgi
+        self.cgi_info = _url_collapse_path_split(self.path)
+        return True
+        #is_cgi = CGIHTTPServer.CGIHTTPRequestHandler.is_cgi(self)
+        #if is_cgi == True:
+        #    pathname = '.' + self.path.split("?")[0]
+        #    is_cgi = os.path.isfile(pathname) and self.is_executable(pathname)
+        #return is_cgi
 
 server = BaseHTTPServer.HTTPServer(('', 8122), ServerHandler)
 certfile = os.path.dirname(sys.argv[0]) + '/server.pem'
