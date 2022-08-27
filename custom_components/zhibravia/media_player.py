4fec236db439de93eeb6f48f175bb7eb8d487b8f
Yonsm <Yonsm@qq.com>
2022-08-27 09:50:46 +0800

    Create remote auto

--- custom_components/zhibravia/media_player.py
+++ custom_components/zhibravia/media_player.py
@@ -1,7 +1,9 @@
 
 from homeassistant.components.braviatv.media_player import BraviaTVMediaPlayer
+from homeassistant.helpers import discovery
 from . import async_add_bravia_entity
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     await async_add_bravia_entity(hass, config, async_add_entities, BraviaTVMediaPlayer)
+    await discovery.async_load_platform(hass, 'remote', 'zhibravia', config, {})
--- custom_components/zhibravia/remote.py
+++ custom_components/zhibravia/remote.py
@@ -3,4 +3,4 @@ from . import async_add_bravia_entity
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    await async_add_bravia_entity(hass, config, async_add_entities, BraviaTVRemote)
+    await async_add_bravia_entity(hass, config or discovery_info, async_add_entities, BraviaTVRemote)
