32dc2174484bd9281453fdc1dec90176a58e1ea7
Yonsm <Yonsm@qq.com>
2022-08-27 17:02:41 +0800

    Add zhi broadlink extension; Refine icon & device_class

---
+++ custom_components/zhibroad/__init__.py
@@ -0,0 +1,17 @@
+
+# from homeassistant.components.broadlink import async_setup_entry
+# from homeassistant.config_entries import ConfigEntry
+# DOMAIN = 'zhibroad'
+
+
+# async def async_setup(hass, config):
+#     conf = config.get(DOMAIN)
+#     data = {
+#         'host': conf['host'],
+#         'mac': conf['mac'],
+#         'type': conf['type'],
+#         'timeout': 5,
+#     }
+#     entry = ConfigEntry(version=1, domain='broadlink', entry_id='122333xxx', unique_id='313332', title=conf['name'], data=data, source='user')
+
+#     return await async_setup_entry(hass, entry)
