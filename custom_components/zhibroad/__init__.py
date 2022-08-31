ca9cebb7c940d7d1ec03a78e7bbecdbd34146570
Yonsm <Yonsm@qq.com>
2022-08-31 10:14:51 +0800

    Refine zhibroad

--- custom_components/zhibroad/__init__.py
+++ custom_components/zhibroad/__init__.py
@@ -1,3 +1,4 @@
+from json import load
 from homeassistant.const import CONF_NAME, CONF_HOST, CONF_MAC, CONF_TYPE, CONF_TIMEOUT
 from homeassistant.config_entries import ConfigEntry, SOURCE_USER
 from homeassistant.util import slugify
@@ -12,12 +13,16 @@ async def async_setup(hass, config):
         if hass.config_entries.async_get_entry(unique_id):
             continue
 
-        device = await hass.async_add_executor_job(blk.hello, conf[CONF_HOST], 80, 5)
-        data = {
-            CONF_HOST: device.host[0],
-            CONF_MAC: device.mac.hex(),
-            CONF_TYPE: device.devtype,
-            CONF_TIMEOUT: device.timeout,
-        }
-        await hass.config_entries.async_add(ConfigEntry(1, 'broadlink', name, data, SOURCE_USER, unique_id=unique_id, entry_id=unique_id))
+        try:
+            device = await hass.async_add_executor_job(blk.hello, conf[CONF_HOST], 80, 5)
+            data = {
+                CONF_HOST: device.host[0],
+                CONF_MAC: device.mac.hex(),
+                CONF_TYPE: device.devtype,
+                CONF_TIMEOUT: device.timeout,
+            }
+            await hass.config_entries.async_add(ConfigEntry(1, 'broadlink', name, data, SOURCE_USER, unique_id=unique_id, entry_id=unique_id))
+        except Exception as e:
+            import logging
+            logging.getLogger(__name__).error("Could not find %s", name)
     return True
