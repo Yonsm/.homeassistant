09fd267d0d5d57c4aacc00a2df0354ed5ee98dca
Yonsm <Yonsm@qq.com>
2022-11-14 12:35:07 +0800

    samba

--- extras/setup/N1.sh
+++ extras/setup/N1.sh
@@ -208,7 +208,28 @@ EOF
 systemctl --system daemon-reload
 systemctl enable socks5
 
+
 exit 0
+
+
+# ============================== Samba ==============================
+
+hdparm -I /dev/sda
+hdparm -B 127 /dev/sda
+hdparm -S 180 /dev/sda
+hdparm -I /dev/sda
+hdparm -C /dev/sda
+echo -e "LABEL=STORE\t/mnt/STORE\tntfs\t\tdefaults,noatime,nodiratime\t\t\t0 0" >> /etc/fstab
+#reboot
+
+apt install samba samba-vfs-modules
+smbpasswd -a admin
+cat <<\EOF > /etc/samba/smb.conf
+# ?
+EOF
+
+#smbd -F --no-process-group -S -d=3
+/etc/init.d/smbd restart
 
 # ============================== Install on N1 CoreElec ==============================
 # 
