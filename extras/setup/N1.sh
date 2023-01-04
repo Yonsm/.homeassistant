086c4a07411c7e7410759854b9e9894a4cc36bef
Yonsm <Yonsm@qq.com>
2023-01-04 17:55:57 +0800

    Update to 2012.12.8

--- extras/setup/N1.sh
+++ extras/setup/N1.sh
@@ -264,3 +264,21 @@ pip3 install wheel homeassistant
 systemctl mask kodi
 systemctl start kodi
 systemctl stop kodi
+
+
+# docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=Asia/Shanghai -v /home/hass/config:/config --network=host ghcr.io/home-assistant/home-assistant:stable
+
+# Home Assistant Core Fix
+sed -i 's/_LOGGER.warning(CUSTOM_WARNING/#LOGGER.warning(CUSTOM_WARNING/' /usr/src/homeassistant/homeassistant/loader.py
+sed -i 's/minutes=30/days=30/' /usr/src/homeassistant/homeassistant/auth/const.py
+sed -i 's/ATTERY_MODELS:/ATTERY_MODELS and False:/' /usr/src/homeassistant/homeassistant/components/xiaomi_aqara/sensor.py
+sed -i 's/await hass.config_entries.async_forward_entry_setups/#wait hass.config_entries.async_forward_entry_setups/' /usr/src/homeassistant/homeassistant/components/mobile_app/__init__.py
+
+sed -i 's/Platform.BUTTON/#latform.BUTTON/' /usr/src/homeassistant/homeassistant/components/braviatv/__init__.py
+sed -i 's/f"{ATTR_MANUFACTURER} {model}"/model/' /usr/src/homeassistant/homeassistant/components/braviatv/entity.py
+#sed -i 's/await self.coordinator.async_turn_off()/await self.coordinator.async_turn_off(); await self.coordinator.async_turn_off()/' /usr/src/homeassistant/homeassistant/components/braviatv/media_player.py
+
+
+#sed -i 's/f"{device.name} Remote"/device.name/' /usr/src/homeassistant/homeassistant/components/broadlink/remote.py
+#sed -i 's/f"{device.name} Switch"/device.name/' /usr/src/homeassistant/homeassistant/components/broadlink/switch.py
+sed -i 's/"RM4PRO", "RM4MINI"/"RM4PRO", "RMPRO", "RM4MINI"/' /usr/src/homeassistant/homeassistant/components/broadlink/sensor.py
