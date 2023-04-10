9cf43de570d74077bbbaaeb2c85445135a9d835f
Yonsm <Yonsm@qq.com>
2023-04-10 17:22:24 +0800

    Remove zhimqtt (error on 2023.4.1); Fix into mqtt.switch

--- extras/setup/HassFix.sh
+++ extras/setup/HassFix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cd /usr/lib/python3.10/site-packages/homeassistant
 cd /usr/src/homeassistant/homeassistant
+cd /usr/lib/python3.10/site-packages/homeassistant
 
 sed -i 's/_LOGGER.warning(CUSTOM_WARNING/#LOGGER.warning(CUSTOM_WARNING/' loader.py
 sed -i 's/minutes=30/days=30/' auth/const.py
@@ -11,3 +11,11 @@ sed -i 's/Platform.BUTTON/#latform.BUTTON/' components/braviatv/__init__.py
 sed -i 's/f"{ATTR_MANUFACTURER} {model}"/model/' components/braviatv/entity.py
 
 sed -i 's/"RM4PRO", "RM4MINI"/"RM4PRO", "RMPRO", "RM4MINI"/' components/broadlink/sensor.py
+
+sed -i 's/await hass.config_entries.async_forward_entry_setups/#wait hass.config_entries.async_forward_entry_setups/' components/sun/__init__.py
+sed -i 's/await hass.config_entries.async_unload_platforms/True or await hass.config_entries.async_unload_platforms/' components/sun/__init__.py
+
+grep 'CONF_ICON_TEMPLATE,' components/mqtt/switch.py || sed -i 's/CONF_VALUE_TEMPLATE,/CONF_VALUE_TEMPLATE, CONF_ICON_TEMPLATE,/' components/mqtt/switch.py
+grep 'CONF_ICON_TEMPLATE)' components/mqtt/switch.py || sed -i 's/cv.template,/cv.template, vol.Optional(CONF_ICON_TEMPLATE): cv.template,/'  components/mqtt/switch.py
+grep '"original_state": m' components/mqtt/switch.py || sed -i 's/payload = self._value_template/if CONF_VALUE_TEMPLATE in self._config: self._attr_state_attributes = {"original_state": msg.payload}\n            if CONF_ICON_TEMPLATE in self._config: self._attr_icon = self._config[CONF_ICON_TEMPLATE].async_render_with_possible_json_value(msg.payload)\n            payload = self._value_template/'  components/mqtt/switch.py
+grep 'state_attributes(s,' components/mqtt/switch.py || sed -i 's/def _prepare_subscribe_topics/@property\n    def state_attributes(self): return self._attr_state_attributes if hasattr(self, "_attr_state_attributes") else None\n\n    def _prepare_subscribe_topics/' components/mqtt/switch.py
